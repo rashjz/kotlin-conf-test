821f3ed3a5871e8ceb124c2dcd548a03a872fe17
Rashad Javadov <rashjz@users.noreply.github.com>
2018-09-04 16:38:09 +0200

    Update StringManipulate.java

--- src/main/java/app/test/arrays/StringManipulate.java
+++ src/main/java/app/test/arrays/StringManipulate.java
@@ -1,8 +1,19 @@
 package app.test.arrays;
 
+import java.util.Arrays;
 
 public class StringManipulate {
+    
+ public static void main(String[] args) {
+        String str = "  131  475709  01    ";
 
+        String result = Arrays.stream(str.split(" "))
+                .map(StringManipulate::sortStringAlphabetically)
+                .reduce((a, b) -> a +" "+ b)
+                .orElse("");
+        System.out.println(result);
+    }
+    
     static String sortStringAlphabetically(String str) {
         return str.chars()
                 .mapToObj(e -> (char) e)
